1eb99ffbab67dffe484bd5df154ba126b5cb88ca
a-einstein <aeinstein@xs4all.nl>
2021-04-21 16:02:16 +0200

    Add CleVR example.

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -20,6 +20,22 @@ namespace Sudoku
 
             if (SolveSudoku(puzzle, 0, 0))
                 PrintSudoku(puzzle);
+
+            // CleVR
+            int[,] puzzleCleVR = {
+                {0,0,0,7,0,4,0,0,5},
+                {0,2,0,0,1,0,0,7,0},
+                {0,0,0,0,8,0,0,0,2},
+                {0,9,0,0,0,6,2,5,0},
+                {6,0,0,0,7,0,0,0,8},
+                {0,5,3,2,0,0,0,1,0},
+                {4,0,0,0,9,0,0,0,0},
+                {0,3,0,0,6,0,0,9,0},
+                {2,0,0,4,0,7,0,0,0}
+            };
+
+            if (SolveSudoku(puzzleCleVR, 0, 0))
+                PrintSudoku(puzzleCleVR);
         }
 
         public static void PrintSudoku(int[,] puzzle)
