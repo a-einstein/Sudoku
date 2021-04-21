ce480cb37d4d4a429521dcf01dc14a4aad120bc2
a-einstein <aeinstein@xs4all.nl>
2021-04-21 18:52:11 +0200

    Layout. Refactoring names. Use direct figures. Debugging.

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -6,7 +6,9 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            int[][,] puzzles = new int[][,]                {
+            int[][,] puzzles = new int[][,] 
+            {
+                /*
                 new int[,] {
                     { 3, 2, 1, 7, 0, 4, 0, 0, 0 },
                     { 6, 4, 0, 0, 9, 0, 0, 0, 7 },
@@ -18,6 +20,7 @@ namespace Sudoku
                     { 2, 0, 0, 0, 7, 0, 0, 1, 9 },
                     { 0, 0, 0, 6, 0, 9, 5, 8, 2 }
                 },
+                */
 
                 // CleVR
                new int[,] {
@@ -41,8 +44,8 @@ namespace Sudoku
 
         private static void Handle(int[,] puzzle)
         {
-            var timeStart= DateTime.Now;
-            var solved = SolveSudoku(puzzle, 0, 0);
+            var timeStart = DateTime.Now;
+            var solved = SolveCell(puzzle, 0, 0);
             var duration = DateTime.Now - timeStart;
 
             Console.WriteLine();
@@ -71,54 +74,95 @@ namespace Sudoku
             }
         }
 
-        public static bool SolveSudoku(int[,] puzzle, int row, int col)
+        public static bool SolveCell(int[,] puzzle, int cellRow, int cellColumn)
         {
-            if (row < 9 && col < 9)
+            // Not completed.
+            if (cellRow < 9 && cellColumn < 9)
             {
-                if (puzzle[row, col] != 0)
+                // Cell HAS value.
+                if (puzzle[cellRow, cellColumn] != 0)
                 {
-                    if ((col + 1) < 9) return SolveSudoku(puzzle, row, col + 1);
-                    else if ((row + 1) < 9) return SolveSudoku(puzzle, row + 1, 0);
-                    else return true;
+                    // Next column.
+                    if ((cellColumn + 1) < 9)
+                        return SolveCell(puzzle, cellRow, cellColumn + 1);
+
+                    // Next row.
+                    else if ((cellRow + 1) < 9)
+                        return SolveCell(puzzle, cellRow + 1, 0);
+
+                    else
+                        return true;
                 }
+                // Cell has NO value.
                 else
                 {
-                    for (int i = 0; i < 9; ++i)
+                    for (int figure = 1; figure <= 9; ++figure)
                     {
-                        if (IsAvailable(puzzle, row, col, i + 1))
+                        if (FigureAvailable(puzzle, cellRow, cellColumn, figure))
                         {
-                            puzzle[row, col] = i + 1;
+                            puzzle[cellRow, cellColumn] = figure;
 
-                            if ((col + 1) < 9)
+                            if ((cellColumn + 1) < 9)
                             {
-                                if (SolveSudoku(puzzle, row, col + 1)) return true;
-                                else puzzle[row, col] = 0;
+                                if (SolveCell(puzzle, cellRow, cellColumn + 1))
+                                    return true;
+                                else
+                                    puzzle[cellRow, cellColumn] = 0;
                             }
-                            else if ((row + 1) < 9)
+                            else if ((cellRow + 1) < 9)
                             {
-                                if (SolveSudoku(puzzle, row + 1, 0)) return true;
-                                else puzzle[row, col] = 0;
+                                if (SolveCell(puzzle, cellRow + 1, 0))
+                                    return true;
+                                else
+                                    puzzle[cellRow, cellColumn] = 0;
                             }
-                            else return true;
+                            else
+                                return true;
                         }
                     }
                 }
 
                 return false;
             }
-            else return true;
+            else
+                return true;
         }
 
-        private static bool IsAvailable(int[,] puzzle, int row, int col, int num)
+        // TODO remove if not needed.
+        struct Cell
         {
-            int rowStart = (row / 3) * 3;
-            int colStart = (col / 3) * 3;
+            public int Row;
+            public int Column;
+        }
+
+        private static bool FigureAvailable(int[,] puzzle, int cellRow, int cellColumn, int figure)
+        {
+            // Debug.
+            Console.WriteLine();
+            Console.WriteLine($"Cell({cellRow},{cellColumn}), {nameof(figure)} = {figure}");
+
+            int boxStartRow = (cellRow / 3) * 3;
+            int boxStartColumn = (cellColumn / 3) * 3;
 
             for (int i = 0; i < 9; ++i)
             {
-                if (puzzle[row, i] == num) return false;
-                if (puzzle[i, col] == num) return false;
-                if (puzzle[rowStart + (i % 3), colStart + (i / 3)] == num) return false;
+                // Vary column at cellRow.
+                if (puzzle[cellRow, i] == figure)
+                    return false;
+
+                // Vary row at cellColumn.
+                if (puzzle[i, cellColumn] == figure)
+                    return false;
+
+                // TODO This repeats 3 times and does not cover the whole box!
+                var boxCellRow = boxStartRow + (i % 3);
+                var boxCellColumn = boxStartColumn + (i / 3);
+
+                // Debug.
+                Console.WriteLine($"{nameof(i)} = {i}, boxCell({boxCellRow},{boxStartColumn})");
+
+                if (puzzle[boxCellRow, boxCellColumn] == figure)
+                    return false;
             }
 
             return true;
