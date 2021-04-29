1c4c4d69644a7db9f058a7e22079fa7706521de9
a-einstein <aeinstein@xs4all.nl>
2021-04-29 21:47:33 +0200

    Get data into table.

--- App2/ViewModels/DataGridViewModel.cs
+++ App2/ViewModels/DataGridViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using App2.Contracts.ViewModels;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
+using Sudoku;
 using System.Data;
 
 namespace App2.ViewModels
@@ -10,7 +11,7 @@ namespace App2.ViewModels
         [PreferredConstructor]
         public DataGridViewModel()
         {
-            Init();
+            InitTable();
         }
 
         public DataView Source { get; set; }
@@ -18,13 +19,13 @@ namespace App2.ViewModels
         static DataTable table;
         static DataView view;
 
-        private void Init()
+        private void InitTable()
         {
             table = new DataTable();
 
             // Impovised numbering of rows. Needs name.
             // Note this changes indexing!
-            table.Columns.Add(new DataColumn("rij", typeof(char)));
+            //table.Columns.Add(new DataColumn("rij", typeof(char)));
 
             table.Columns.Add(new DataColumn("a", typeof(int)));
             table.Columns.Add(new DataColumn("b", typeof(int)));
@@ -36,15 +37,15 @@ namespace App2.ViewModels
             table.Columns.Add(new DataColumn("h", typeof(int)));
             table.Columns.Add(new DataColumn("i", typeof(int)));
 
-            table.Rows.Add('a', 1, 2, 3, 4, 5, 6, 7, 8, 9);
-            table.Rows.Add('b', 2, 3, 4, 5, 6, 7, 8, 9, 1);
-            table.Rows.Add('c', 3, 4, 5, 6, 7, 8, 9, 1, 2);
-            table.Rows.Add('d', 4, 5, 6, 7, 8, 9, 1, 2, 3);
-            table.Rows.Add('e', 5, 6, 7, 8, 9, 1, 2, 3, 4);
-            table.Rows.Add('f', 6, 7, 8, 9, 1, 2, 3, 4, 5);
-            table.Rows.Add('g', 7, 8, 9, 1, 2, 3, 4, 5, 6);
-            table.Rows.Add('h', 8, 9, 1, 2, 3, 4, 5, 6, 7);
-            table.Rows.Add('i', 9, 1, 2, 3, 4, 5, 6, 7, 8);
+            //table.Rows.Add('a', 1, 2, 3, 4, 5, 6, 7, 8, 9);
+            //table.Rows.Add('b', 2, 3, 4, 5, 6, 7, 8, 9, 1);
+            //table.Rows.Add('c', 3, 4, 5, 6, 7, 8, 9, 1, 2);
+            //table.Rows.Add('d', 4, 5, 6, 7, 8, 9, 1, 2, 3);
+            //table.Rows.Add('e', 5, 6, 7, 8, 9, 1, 2, 3, 4);
+            //table.Rows.Add('f', 6, 7, 8, 9, 1, 2, 3, 4, 5);
+            //table.Rows.Add('g', 7, 8, 9, 1, 2, 3, 4, 5, 6);
+            //table.Rows.Add('h', 8, 9, 1, 2, 3, 4, 5, 6, 7);
+            //table.Rows.Add('i', 9, 1, 2, 3, 4, 5, 6, 7, 8);
 
             Source = view = table.DefaultView;
         }
@@ -53,18 +54,28 @@ namespace App2.ViewModels
         {
             //Source.Clear();
 
-            //foreach (var item in data)
-            //{
-            //    Source.Add(item);
-            //}
+            if (Puzzle.Read())
+            {
+                ConvertToTable(Puzzle.Grid);
 
-            //table.Rows[1].ItemArray[1] = 0;
-            //table.Rows[1].Field<int>(1) = 0;
-            //table.Field<int>(1,1) = 0;
+                //Puzzle.Handle();
+            }
+        }
 
-            // Expriments on binding.
-            view[1].Row[8] = 0;
-            table.Rows[2][8] = 0;
+        private static void ConvertToTable(int [][] grid)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                var newRow = table.NewRow();
+
+                // Unfortunately assigning to ItemArray does not work for int[].
+                for (int column = 0; column < 9; column++)
+                {
+                    newRow[column] = grid[row][column];
+                }
+
+                table.Rows.Add(newRow);
+            }
         }
 
         public void OnNavigatedFrom()
