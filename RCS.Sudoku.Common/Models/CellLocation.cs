3a03f6f7d6d5d7cd2622d0580dce1f6f193e4ed9
a-einstein <aeinstein@xs4all.nl>
2021-07-26 22:16:22 +0200

    Make CellLocation a record.

--- RCS.Sudoku.Common/Models/CellLocation.cs
+++ RCS.Sudoku.Common/Models/CellLocation.cs
@@ -1,17 +1,9 @@
 ﻿namespace RCS.Sudoku.Common.Models
 {
     /// <summary>
-    /// Helper structure to compactly store cell location.
+    /// Helper container to compactly store cell location.
     /// </summary>
-public struct CellLocation
-    {
-        public int RowIndex;
-        public int ColumnIndex;
-
-        public CellLocation(int rowIndex, int columnIndex)
-        {
-            RowIndex = rowIndex;
-            ColumnIndex = columnIndex;
-        }
-    }
+    /// 
+    // Note this record syntax creates init-only auto-implemented properties.
+    public record CellLocation(int RowIndex, int ColumnIndex);
 }
